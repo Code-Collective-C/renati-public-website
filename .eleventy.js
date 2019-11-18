9e7972a33f1669221ab053a27e4f0d98764e3a7c
Steve Barnett <steve@naga.co.za>
2019-11-18 17:55:43 +0200

    Add CMS POC

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,14 @@
-module.exports = function (eleventyConfig) {
+module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("fonts");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("admin/config.yml");
+
+  eleventyConfig.addFilter("markdownify", function(content) {
+    var MarkdownIt = require("markdown-it");
+    var md = new MarkdownIt();
+    var result = md.render(content);
+    return result;
+  });
 };
